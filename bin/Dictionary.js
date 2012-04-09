62d6298db480f263db9e57fd2ab7974a346aca6f
Paul Taylor <guyinthechair@gmail.com>
2012-04-08 21:42:08 -0500

    Adds the "useObservable" method to Dictionary to allow components to modify how and why they dispatch values.
    For example, a component may define an "index" observable property. The component could check the index's boundaries for valid values so it doesn't broadcast an invalid index by calling "useObservable" with "index" and a modified observable value.
    
    example:
    	@useObservable, 'index', @define('index', -1).where((i) -> i > -1)

--- bin/Dictionary.js
+++ bin/Dictionary.js
@@ -3,20 +3,22 @@ var Dictionary;
 Dictionary = (function() {
 
   function Dictionary() {
-    var setObservable, setVal, subjects;
+    var observables, setObservable, setVal, subjects;
     var _this = this;
+    observables = {};
     subjects = {};
     this.define = function(name, initial) {
-      if (initial !== void 0 && (initial instanceof Rx.Observable)) {
-        setObservable(name, initial.publish());
-      }
-      if (initial === void 0 || (initial instanceof Rx.Observable) === false) {
-        setVal(name, initial);
-      }
       _this.__defineGetter__(name, function() {
-        return subjects[name];
+        return observables[name];
       });
-      return _this[name];
+      if (initial !== void 0 && (initial instanceof Rx.Observable)) {
+        return setObservable(name, initial.publish());
+      } else {
+        return setVal(name, initial);
+      }
+    };
+    this.useObservable = function(name, observable) {
+      return observables[name] = observable;
     };
     setObservable = function(name, observable) {
       var disposable;
@@ -24,16 +26,23 @@ Dictionary = (function() {
       _this.__defineSetter__(name, function(val) {
         disposable.dispose();
         disposable = (observable = val.publish()).connect();
-        subjects[name] = observable;
+        return subjects[name] = observable;
       });
+      return subjects[name] = observables[name] = observable;
     };
     setVal = function(name, initial) {
-      subjects[name] = new Rx.BehaviorSubject(initial);
       _this.__defineSetter__(name, function(val) {
-        subjects[name].onNext(val);
+        return subjects[name].onNext(val);
       });
+      return subjects[name] = observables[name] = new Rx.BehaviorSubject(initial);
     };
   }
+
+  Dictionary.inc = 1;
+
+  Dictionary.prototype.getUUID = function() {
+    return Math.round(Math.random() * new Date().getTime() + ++Dictionary.inc, Math.pow(10, 10));
+  };
 
   return Dictionary;
 
--- src/Dictionary.coffee
+++ src/Dictionary.coffee
@@ -1,24 +1,28 @@
 class Dictionary
 	constructor: () ->
+		observables = {}
 		subjects = {}
 		
 		@define = (name, initial) =>
-			setObservable name, initial.publish() if(initial != undefined and (initial instanceof Rx.Observable))
-			setVal name, initial if(initial == undefined or (initial instanceof Rx.Observable) == false)
-			@__defineGetter__ name, () -> subjects[name]
-			@[name]
+			@__defineGetter__ name, () -> observables[name]
+			if(initial != undefined and (initial instanceof Rx.Observable))
+				setObservable name, initial.publish() 
+			else
+				setVal name, initial
+		
+		@useObservable = (name, observable) => observables[name] = observable
 		
 		setObservable = (name, observable) =>
 			disposable = observable.connect()
 			@__defineSetter__ name, (val) -> 
 				disposable.dispose()
 				disposable = (observable = val.publish()).connect()
-				subjects[name] = observable;
-				return
-			return
+				subjects[name] = observable
+			subjects[name] = observables[name] = observable;
 		
 		setVal = (name, initial) =>
-			subjects[name] = new Rx.BehaviorSubject(initial)
-			@__defineSetter__ name, (val) -> subjects[name].onNext val; return
-			return
+			@__defineSetter__ name, (val) -> subjects[name].onNext val
+			subjects[name] = observables[name] = new Rx.BehaviorSubject(initial)
 	
+	@inc: 1
+	getUUID: () -> Math.round Math.random() * new Date().getTime() + ++Dictionary.inc, Math.pow 10, 10
